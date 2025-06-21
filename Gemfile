b1a486cfebf75579dda17191b0e500ed7d56e277
takuya.t <shoori0621@gmail.com>
2025-06-21 06:08:07 +0000

    setup project

--- Gemfile
+++ Gemfile
@@ -40,6 +40,18 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+# dotenv
+gem 'dotenv-rails'
+
+# Use Redis adapter to run Action Cable in production
+gem 'redis', '>= 4.0.1'
+# redisの高速化
+gem 'hiredis'
+
+# sidekiq
+gem 'sidekiq'
+
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -17,12 +17,18 @@ Rails.application.configure do
 
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-    config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
+
+    config.cache_store = config.cache_store = :redis_cache_store, {
+      host: ENV['REDIS_HOST'],
+      port: ENV['REDIS_PORT'],
+      driver: :hiredis
+    }
   else
     config.action_controller.perform_caching = false
+    config.cache_store = :null_store
   end
 
   # Change to :null_store to avoid any caching.
@@ -38,7 +44,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
